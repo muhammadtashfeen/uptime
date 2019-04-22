04b5d255a3eab9aae79d10a32e4a3c6af3b1eb90
Muhammad Tashfeen <muhammad.tashfeen.amin@gmail.com>
2019-04-22 19:49:48 +0200

    Remove database value from database.php and add to .env

--- config/database.php
+++ config/database.php
@@ -42,11 +42,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => 'localhost',
-            'port' => '33060',
-            'database' => 'assets',
-            'username' => 'homestead',
-            'password' => 'secret',
+            'host' => env("DB_HOST", "localhost"),
+            'port' => env("DB_PORT", "3306"),
+            'database' => env("DB_DATABASE", "database"),
+            'username' => env("DB_USERNAME", "username"),
+            'password' => env("DB_PASSWORD", "password"),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
